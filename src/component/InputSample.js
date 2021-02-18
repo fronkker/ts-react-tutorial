575ba01e755ca4aaeeb2f21b2cb26f8cd6043c16
Hanbeom Seo <vumbumy@outlook.kr>
2021-02-18 17:47:19 +0900

    9. 여러개의 input 상태 관리하기

--- src/component/InputSample.js
+++ src/component/InputSample.js
@@ -11,12 +11,42 @@ function InputSample() {
     setText('');
   };
 
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+
+  const onChangeMulti = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
+  };
+
+  const onResetMulti = () => {
+    setInputs({
+      name: '',
+      nickname: '',
+    })
+  };
+
   return (
     <div>
       <input onChange={onChange} value={text}/>
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값: {text}</b>
+      </div>
+
+      <input name="name" placeholder="이름" onChange={onChangeMulti} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChangeMulti} value={nickname}/>
+      <button onClick={onResetMulti}>초기화</button>
+      <div>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
